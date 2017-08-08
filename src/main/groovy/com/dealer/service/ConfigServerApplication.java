c15dbb48839d62388ffae12cc2be4c9c5797d199
Patrick Greer (Team Velociraptors) <patrick.greer@coxautoinc.com>
2017-08-08 11:49:06 -0500

    initial config server

---
+++ src/main/groovy/com/dealer/service/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.dealer.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] arguments) {
+        SpringApplication.run(ConfigServerApplication.class, arguments);
+    }
+
+
+
+}
